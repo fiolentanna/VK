ebb93a33e5c719848a556b5c3f81d397527cf972
fiolentanna <tigranka@gmail.com>
2021-05-09 02:55:48 +0300

    Added manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post [] posts;
+
+    public Post [] search (int owner_id, String domain, String query, boolean ownersOnly, int count, int offset){
+        //some logic
+        return null;
+    }
+
+    public Post [] delete (int ownerId, int postId){
+        //some logic
+        return null;
+    }
+}
