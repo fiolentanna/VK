fa1456164c196a76f909c464f4f68922fe1e8e83
fiolentanna <tigranka@gmail.com>
2021-05-09 00:54:06 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+public class Post {
+    private int postId;
+    private int ownerAccId;
+    private String accountAvatar;
+    private String text;
+    private String postPicUrl;
+    private int date;
+    private int likes;
+    private int comments;
+    private int reposts;
+    private int views;
+    private String postType;
+
+
+    // + getters/setters
+}
